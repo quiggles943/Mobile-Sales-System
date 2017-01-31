1b5288b78b937cd21eb116e4d7d453b9d6503bb4
Josh R <40114815@live.napier.ac.uk>
2017-01-31 11:36:06 +0000

    inclusion of orderItem class

---
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/orderItem.java
@@ -0,0 +1,58 @@
+package com.example.mss.mobilesalessystem;
+
+/**
+ * Created by 40114815 on 31/01/2017.
+ */
+public class orderItem {
+
+    private int itemID;                         //int to hold the item ID, used for database lookup
+    private String itemDescription;             //String to hold the description of the item
+    private String itemSize;                    //String to hold the size of the item, i.e. A4, A5, Keyring
+    private Float itemPrice;                    //Float to hold the price of the item, due to decimal place acceptance
+
+    public orderItem() {
+        //standard constructor
+    }
+
+    public orderItem (int itemID, String itemDescription, String itemSize, Float itemPrice)
+    {
+        this.itemID = itemID;
+        this.itemDescription = itemDescription;
+        this.itemSize = itemSize;
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemSize() {
+        return itemSize;
+    }
+
+    public void setItemSize(String itemSize) {
+        this.itemSize = itemSize;
+    }
+
+    public Float getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Float itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+
+}

35020e0559e8e7bc108f05eb46090fd6181d1d4b
Paul Quigley <40163180@live.napier.ac.uk>
2017-02-01 22:51:43 +0000

    Merged Design Branch to Experimental. Basic QR Scanner Implemented

--- MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/MainMenu.java
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/MainMenu.java
@@ -1,15 +1,39 @@
 package com.example.mss.mobilesalessystem;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.pm.ActivityInfoCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Kriss on 30/01/2017.
  */
 public class MainMenu extends Activity {
+    Context context;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.mainmenu_layout);
+        ImageButton transactionBtn = (ImageButton) findViewById(R.id.imageButton);
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
+        transactionBtn.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View view) {
+                                                  startActivity(new Intent(context,qrScanner.class));
+                                              }
+                                          }
+        );
     }
 }
--- MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/qrReader.java
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/qrReader.java
@@ -1,30 +1,46 @@
 package com.example.mss.mobilesalessystem;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
 /**
  * Created by 40163180 on 27/01/2017.
  */
-//public class qrReader extends ActionBarActivity {
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        try {
-//            Bitmap myQRCode = BitmapFactory.decodeStream(
-//                    getAssets().open("hw_qr.png")
-//            );
-//            BarcodeDetector barcodeDetector =
-//                    new BarcodeDetector.Builder(this)
-//                            .setBarcodeFormats(Barcode.QR_CODE)
-//                            .build();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//}
+public class qrReader extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Bitmap myQRCode = BitmapFactory.decodeResource(this.getResources(),R.drawable.hw_qr);
+            BarcodeDetector barcodeDetector =
+                    new BarcodeDetector.Builder(this)
+                            .setBarcodeFormats(Barcode.QR_CODE)
+                            .build();
+            Frame myFrame = new Frame.Builder()
+                    .setBitmap(myQRCode)
+                    .build();
+            SparseArray<Barcode> barcodes = barcodeDetector.detect(myFrame);
+            if(barcodes.size() != 0)
+            {
+                Log.d("QR Code data",barcodes.valueAt(0).displayValue);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}

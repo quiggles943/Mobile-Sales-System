fe81a06200d4a41d8d3a839d704acda6f68f8fa1
JRLAPTOP <mrjbean1110_1@hotmail.co.uk>
2017-02-08 14:02:51 +0000

    Updates to the SQL schema inputted

--- MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/MainMenu.java
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/MainMenu.java
@@ -47,13 +47,19 @@ public class MainMenu extends Activity {
             String [] tableColumns = new String[5];
 
             tableName[2] = "product";
-            tableColumns[2] = "Format text, ImageID text, ProdDesc text, Price REAL";
+            tableColumns[2] = "Format text, ImageID text, ProdDesc text, Price REAL, PRIMARY KEY (Format, ImageID)";
 
             tableName[1] = "image";
-            tableColumns[1] = "ImageID text, ImageDesc text, ImgFilePath text";
+            tableColumns[1] = "ImageID text, ImageDesc text, ImgFilePath text, PRIMARY KEY (ImageID)";
 
             tableName[0] = "format";
-            tableColumns[0] = "Format text, FormatDesc text";
+            tableColumns[0] = "Format text, FormatDesc text, PRIMARY KEY (Format)";
+
+            tableName[3] = "invoice";
+            tableColumns[3] = "InvoiceID INTEGER, Date DATE, Event INTEGER, CustomerID INTEGER";
+
+            tableName[4] = "invoiceitems";
+            tableColumns[4] = "InvoiceID INTEGER, ProductID INTEGER, Qty INTEGER";
 
             pDB = context.openOrCreateDatabase("ProductDB", MODE_PRIVATE, null);
 

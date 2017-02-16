877f60e361fb8a722b47d6cc5fd36abccc4e0bf3
KrissYeah <chris-zmar@web.de>
2017-02-16 19:53:21 +0100

    - added layouts

--- MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/MainMenu.java
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/MainMenu.java
@@ -2,14 +2,30 @@ package com.example.mss.mobilesalessystem;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Kriss on 30/01/2017.
  */
 public class MainMenu extends Activity {
+
+    // Mockup Adapter
+    private BaseAdapter adapter;
+    private ListView listview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainmenu_layout);
+        setContentView(R.layout.item_detail_layout);
+
+        //// Mockup listview layout ---> cart_layout
+        //listview = (ListView) findViewById(R.id.lv_itemList);
+        // create new Adapter
+        //adapter = new CustomItemAdapter(this);
+        // set Adapter on listview
+        //listview.setAdapter(adapter);
     }
 }
+

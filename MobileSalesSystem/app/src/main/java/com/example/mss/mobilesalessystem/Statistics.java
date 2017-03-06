c3e10fe8bbf16067e54211d7e7d2ce6ce6d1320d
JRLAPTOP <mrjbean1110_1@hotmail.co.uk>
2017-03-06 16:56:55 +0000

    Change to database connector in order to ensure a connection is available

--- MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/Statistics.java
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/Statistics.java
@@ -1,16 +1,22 @@
 package com.example.mss.mobilesalessystem;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -56,14 +62,36 @@ public class Statistics extends Activity {
         syncFromDb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseConnector connector = new DatabaseConnector(context, new String[]{"product","format","image"});      //creating a new database connector                 //openning up an editor to write to shared preferences
 
-                String token = mSharedPreference.getString("token","");                               //gaining the token
+                ConnectivityManager cManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);          //opening a connectivity manager
+                NetworkInfo wifiInfo = cManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);                                //getting the wifi information
+                NetworkInfo dataInfo = cManager.getNetworkInfo(ConnectivityManager.TYPE_DUMMY);                               //getting the mobile data information
 
-                connector.execute(token);                                                   //running the database connector with the token
+                final DatabaseConnector connector = new DatabaseConnector(context, new String[]{"product","format","image"});      //creating a new database connector                 //openning up an editor to write to shared preferences
+
+                final String token = mSharedPreference.getString("token","");                               //gaining the token
+
+                if(wifiInfo.isAvailable()) {
+                    connector.execute(token);                                                   //running the database connector with the token
+                } else if (dataInfo.isAvailable()) {
+                    new AlertDialog.Builder(new ContextThemeWrapper(context, android.R.style.Theme_Material_Dialog_Alert))
+                            .setTitle("Confirm Database Sync")
+                            .setMessage("You are on mobile data, are you sure you would like to progress?")
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    connector.execute(token);
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, null).show();
+                } else {
+                    Toast.makeText(context, "No internet available", Toast.LENGTH_SHORT).show();        //Toast to say there is no internet at all
+                }
 
             }
         });
     }
+
+
 
 }

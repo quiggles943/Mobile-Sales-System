c9d5fc94e3ca7a9f1d1dffb9df66b553aef530c5
JRLAPTOP <mrjbean1110_1@hotmail.co.uk>
2017-03-07 10:19:09 +0000

    Connectivity checker now working and has warning for user if on mobile data.

--- MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/Statistics.java
+++ MobileSalesSystem/app/src/main/java/com/example/mss/mobilesalessystem/Statistics.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -64,16 +65,19 @@ public class Statistics extends Activity {
             public void onClick(View view) {
 
                 ConnectivityManager cManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);          //opening a connectivity manager
-                NetworkInfo wifiInfo = cManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);                                //getting the wifi information
-                NetworkInfo dataInfo = cManager.getNetworkInfo(ConnectivityManager.TYPE_DUMMY);                               //getting the mobile data information
+                NetworkInfo networkType = cManager.getActiveNetworkInfo();
 
                 final DatabaseConnector connector = new DatabaseConnector(context, new String[]{"product","format","image"});      //creating a new database connector                 //openning up an editor to write to shared preferences
 
                 final String token = mSharedPreference.getString("token","");                               //gaining the token
 
-                if(wifiInfo.isAvailable()) {
+                if(networkType == null)
+                {
+                    Toast.makeText(context,"No internet available", Toast.LENGTH_SHORT).show();        //Toast to say there is no internet at all
+                }
+                else if(networkType.getType() == ConnectivityManager.TYPE_WIFI) {
                     connector.execute(token);                                                   //running the database connector with the token
-                } else if (dataInfo.isAvailable()) {
+                } else if (networkType.getType() == ConnectivityManager.TYPE_MOBILE) {
                     new AlertDialog.Builder(new ContextThemeWrapper(context, android.R.style.Theme_Material_Dialog_Alert))
                             .setTitle("Confirm Database Sync")
                             .setMessage("You are on mobile data, are you sure you would like to progress?")
@@ -84,8 +88,6 @@ public class Statistics extends Activity {
                                 }
                             })
                             .setNegativeButton(android.R.string.no, null).show();
-                } else {
-                    Toast.makeText(context, "No internet available", Toast.LENGTH_SHORT).show();        //Toast to say there is no internet at all
                 }
 
             }
